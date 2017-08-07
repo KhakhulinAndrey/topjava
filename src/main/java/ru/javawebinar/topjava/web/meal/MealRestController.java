9ba7f64c135f5c8e2a42b62c0414c31c31bfe1f3
Andrey Khakhulin <leadtek63@gmail.com>
2017-08-07 21:26:00 +0400

    убрал из сигнатуры методов MealRestController'a userId, беру его из AuthorisedUser прямо в методе

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
@@ -21,35 +22,39 @@ public class MealRestController {
     @Autowired
     private MealService service;
 
-    public List<Meal> getAll(int userId) {
+    public List<Meal> getAll() {
         log.info("getAll");
+        int userId = AuthorizedUser.id();
         return service.getAll(userId);
     }
 
-    public Meal get(int id, int userId) {
+    public Meal get(int id) {
         log.info("get {}", id);
+        int userId = AuthorizedUser.id();
         return service.get(id, userId);
     }
 
-    public Meal create(Meal meal, int userId) {
+    public Meal create(Meal meal) {
         log.info("create {}", meal);
-        checkNew(meal);
+        int userId = AuthorizedUser.id();
         return service.save(meal, userId);
     }
 
-    public void delete(int id, int userId) {
+    public void delete(int id) {
         log.info("delete {}", id);
+        int userId = AuthorizedUser.id();
         service.delete(id, userId);
     }
 
-    public void update(Meal meal, int userId) {
+    public void update(Meal meal) {
+        int userId = AuthorizedUser.id();
         log.info("update {} with id={}", meal, userId);
-        checkIdConsistent(meal, userId);
         service.update(meal, userId);
     }
 
-    public List<Meal> getFiltered(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime, int userId) {
+    public List<Meal> getFiltered(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
         log.info("getFiltred");
+        int userId = AuthorizedUser.id();
         return service.getFiltered(startDate, endDate, startTime, endTime, userId);
     }
 
