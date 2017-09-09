807622670f7b9a73657155c99b38c97e6de7be16
KhakhulinAndrey <leadtek63@gmail.com>
2017-09-09 13:01:12 +0400

    1.3 Починить работу meals: перенести функциональность MealServlet в контроллеры (сервлеты удалить)

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -10,12 +10,17 @@ import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
 public class RootController {
@@ -44,26 +49,57 @@ public class RootController {
     }
 
     @RequestMapping(value = "/meals", method = RequestMethod.GET)
-    public String meals(Model model){
+    public String meals(Model model) {
         model.addAttribute("meals",
                 MealsUtil.getWithExceeded(mealService.getAll(AuthorizedUser.id()), AuthorizedUser.getCaloriesPerDay()));
         return "meals";
     }
 
     @RequestMapping(value = "/delete={id}", method = RequestMethod.GET)
-    public String deleteMeal(@PathVariable("id") int id, Model model)
-    {
+    public String deleteMeal(@PathVariable("id") int id, Model model) {
         mealService.delete(id, AuthorizedUser.id());
-        return meals(model);
+        return "redirect:meals";
     }
 
     @RequestMapping(value = "/update={id}", method = RequestMethod.GET)
-    public String updateDeleteMeal(@PathVariable("id") int id, Model model)
-    {
-        final Meal meal =  (id == -1) ?
+    public String updateDeleteMeal(@PathVariable("id") int id, Model model) {
+        final Meal meal = (id == -1) ?
                 new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
                 mealService.get(id, AuthorizedUser.id());
         model.addAttribute("meal", meal);
-        return "mealForm ";
+        return "mealForm";
+    }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.POST)
+    public String createUpdateMeal(HttpServletRequest request) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        if (request.getParameter("id").isEmpty()) {
+            mealService.create(meal, AuthorizedUser.id());
+        } else {
+            meal.setId(Integer.parseInt(request.getParameter("id")));
+            mealService.update(meal, AuthorizedUser.id());
+        }
+        return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/filter", method = RequestMethod.POST)
+    public String filterMeals(HttpServletRequest request, Model model)
+    {
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        model.addAttribute("meals", MealsUtil.getFilteredWithExceeded(
+                mealService.getBetweenDates(
+                        startDate != null ? startDate : DateTimeUtil.MIN_DATE,
+                        endDate != null ? endDate : DateTimeUtil.MAX_DATE, AuthorizedUser.id()),
+                startTime != null ? startTime : LocalTime.MIN,
+                endTime != null ? endTime : LocalTime.MAX,
+                AuthorizedUser.getCaloriesPerDay()));
+        return "meals";
     }
 }
--- src/main/webapp/WEB-INF/jsp/mealForm.jsp
+++ src/main/webapp/WEB-INF/jsp/mealForm.jsp
@@ -9,7 +9,7 @@
 <body>
 <section>
     <h3><a href="index.html">Home</a></h3>
-    <h2>${param.action == 'create' ? 'Create meal' : 'Edit meal'}</h2>
+    <h2>${param.id == '' ? 'Create meal' : 'Edit meal'}</h2>
     <hr>
     <jsp:useBean id="meal" type="ru.javawebinar.topjava.model.Meal" scope="request"/>
     <form method="post" action="meals">
